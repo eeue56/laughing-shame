c2d4afd42180bb1665375b572adb21d44cb68973
eeue56 <eeue56@bangor.ac.uk>
2014-01-31 16:10:37 +0000

    got video from local storage working

--- laughing-shame/main.cpp
+++ laughing-shame/main.cpp
@@ -9,17 +9,40 @@ using namespace std;
 using namespace cv;
 
 int main(int argc, char * argv[]){
-    bool uses_camera = false;
     const string window_name = "main";
+    VideoCapture video;
+
 
     if (argc < 2){
+        video = VideoCapture(0);
         cout << "Using camera.." << endl;
-        uses_camera = true;
     }
     else {
+        video = VideoCapture(argv[1]);
+        cout << "Using file " << argv[1] << endl;
+    }
 
+    if (!video.isOpened()){
+        cout << "Failed to open video." << endl;
     }
 
     namedWindow(window_name, CV_WINDOW_NORMAL);
+
+    Mat frame;
+    while (true){
+
+       bool has_read = video.read(frame); // read a new frame from video
+
+       if (!has_read){
+           cout << "A reading error occured.";
+           return -1;
+       }
+
+
+       imshow(window_name, frame);
+       waitKey(1);
+    }
+
+    return 0;
 }
 
